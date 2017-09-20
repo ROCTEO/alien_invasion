457df4890c6f08e5b268cce9b90651c2bd9a4cd7
zjp <jinpeng.zhang@outlook.com>
2017-09-20 23:21:55 +0800

    重构屏幕刷新，添加update_screen函数

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,9 +10,9 @@ import game_functions as gf
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
-    ai_setting = Settings()
+    ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_setting.screen_width,ai_setting.screen_height))
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
@@ -23,11 +23,7 @@ def run_game():
         #监视鼠标和键盘事件
         gf.check_events()
 
-        #每次循环时都重绘屏幕
-        screen.fill(ai_setting.bg_color)
-        ship.blitme()
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        #更新屏幕
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()

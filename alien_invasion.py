1a2b37b9f4eb8bde462b4aadddaa4322779577be
zjp <jinpeng.zhang@outlook.com>
2017-09-22 15:23:40 +0800

    创建外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -19,6 +20,9 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
 
+    #创建一个外星人
+    alien = Alien(ai_settings,screen)
+
     #创建一个用于存储子弹的编组
     bullets = Group()
 
@@ -27,14 +31,8 @@ def run_game():
         #监视鼠标和键盘事件
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        bullets.update()
-
-        #删除消失的子弹
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-
+        gf.update_bullet(bullets)
         #更新屏幕
-        gf.update_screen(ai_settings, screen, ship,bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,7 @@ __author__ = 'zjp'
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_event(event,ai_settings,screen,ship,bullets):
     """响应按键"""
@@ -12,10 +13,9 @@ def check_keydown_event(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #创建一颗子弹，并将其加入编组bullets
-        if len(bullets) < ai_settings.bullet_allowed:
-            new_bullet = Bullet(ai_settings,screen,ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_event(event,ship):
     """响应按键"""
@@ -35,7 +35,7 @@ def check_events(ai_settings,screen,ship,bullets):
             check_keyup_event(event,ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -43,6 +43,23 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
+def update_bullet(bullets):
+    """更新子弹位置，并删除消失的子弹"""
+    #更新子弹位置
+    bullets.update()
+
+    # 删除消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    # 创建一颗子弹，并将其加入编组bullets
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)

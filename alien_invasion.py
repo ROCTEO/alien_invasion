4ca5ccd31f15e5347bfbe6ad3b2ea1d532d9835a
zjp <jinpeng.zhang@outlook.com>
2017-09-23 11:45:49 +0800

    创建外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -21,7 +20,8 @@ def run_game():
     ship = Ship(ai_settings,screen)
 
     #创建一个外星人
-    alien = Alien(ai_settings,screen)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #创建一个用于存储子弹的编组
     bullets = Group()
@@ -33,6 +33,6 @@ def run_game():
         ship.update()
         gf.update_bullet(bullets)
         #更新屏幕
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ class Settings():
     def __init__(self):
         """初始化游戏的设置"""
         #屏幕的设置
-        self.screen_width = 900
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230,230,230)
 
         #飞船设置
